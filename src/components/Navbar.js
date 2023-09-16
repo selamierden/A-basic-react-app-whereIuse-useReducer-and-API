a397980355b07cdd8a194180be72c096f9a13c67
selamierdn <erdenselami07@gmail.com>
2023-09-16 16:01:35 +0300

    i have edited navbar

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -10,21 +10,24 @@ function Navbar() {
 
   return (
     <div className="Navbar">
-      <nav className="navbar navbar-expand-lg navbar-light" style={{ backgroundColor: 'black' }}>
-        <a className="navbar-brand" href="/">
-          <img src="/img/etter.png" style={{width:"200px", height: "auto" , paddingLeft: '16px', paddingBottom: '17px' }} className="d-inline-block align-top" alt="" />
+      <nav className="navbar navbar-expand-lg" style={{ backgroundColor: 'black' , padding:"5px" }}>
+        <a className="" href="/">
+          <img src="/img/betteri.png" style={{width:"350px", height: "auto"}} className="d-inline-block align-top" alt="" />
         </a>
         <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav" style={{ fontSize: '20px', fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif' }}>
             <li className="nav-item active">
               <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Home</a>
             </li>
+            <li className="nav-item active">
+              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>News</a>
+            </li>
             <li className="nav-item">
-              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Coin List</a>
+              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Prices</a>
             </li>
             <li className="nav-item dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
               <a className="nav-link dropdown-toggle hvr-underline-from-left" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded={isDropdownOpen} style={{ color: 'white' }}>
-                Spot Transactions
+                Transactions
               </a>
               <div className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`} aria-labelledby="navbarDropdown" style={{backgroundColor: "blue"}}>
                 <a className="dropdown-item" href="/" style={{ color: 'black' }}>Current Transactions</a>
@@ -36,7 +39,7 @@ function Navbar() {
             </li>
           </ul>
         </div>
-        <div className="ml-auto" style={{ fontSize: '21px', fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif' }}>
+        <div className="ml-auto" style={{ fontSize: '20px', fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif' }}>
           <ul className="navbar-nav">
             <li className="nav-item">
               <a className="nav-link hvr-underline-from-left" href="/"style={{ color: 'white' }}>Log In</a>
