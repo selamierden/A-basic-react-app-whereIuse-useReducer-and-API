cbffa3bdea4fcf7ca071ceb07ec5a0b3345070cf
selamierdn <erdenselami07@gmail.com>
2023-09-22 20:46:44 +0300

    react-router

--- src/App.js
+++ src/App.js
@@ -1,19 +1,90 @@
 import React from 'react'
+import ReactDOM from "react-dom";
 import Navbar from './components/Navbar'
 import VideoComponent from './components/VideoComponent'
 import Footer from './components/Footer'
 import MainPrices from './components/MainPrices'
 import CoinList from './components/CoinList'
-import { CurrencyBitcoin } from 'react-bootstrap-icons'
 import CurrentTransaction from './components/CurrentTransaction'
+import { BrowserRouter, Link, Route, Routes } from "react-router-dom";
+import { Nav } from 'react-bootstrap';
 
-function App() {
-  
+const Home = () => {
   return (
     <div>
+      <Navbar></Navbar>
+      <VideoComponent></VideoComponent>
+      <MainPrices></MainPrices>
+      <Footer></Footer>
+    </div>
+  );
+};
+
+const News = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      BURASI NEWS GARDEŞ
+    </div>
+  )
+}
+
+const Prices = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      <CoinList></CoinList>
+    </div>
+  );
+};
+
+const CT = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
       <CurrentTransaction></CurrentTransaction>
     </div>
   )
 }
 
-export default App
+const TR = () => {
+  return (
+    <div>
+       <Navbar></Navbar>
+       BURASI Transaction Records GARDEŞ
+    </div>
+  )
+}
+
+const Contact = () => {
+  return (
+    <div>
+      <Navbar></Navbar>
+      BURASI CONTACT GARDEŞ
+    </div>
+  )
+}
+
+const LoginRegister = () => {
+  return (
+    <div>kayıt alma yeri gardeş</div>
+  )
+}
+
+const App = () => {
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/home" element={<Home />} />
+        <Route path="/news" element={<News />} />
+        <Route path="/prices" element={<Prices />} />
+        <Route path="/ct" element={<CT />} />
+        <Route path="/tr" element={<TR />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="/login-register" element={<LoginRegister />} />
+      </Routes>
+    </BrowserRouter>
+  )
+}
+
+export default App;
--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,4 +1,5 @@
 import React, { useState } from 'react';
+import { Link } from "react-router-dom";
 
 function Navbar() {
   const [isDropdownOpen, setDropdownOpen] = useState(false);
@@ -16,35 +17,35 @@ function Navbar() {
         <div className="collapse navbar-collapse justify-content-center" id="navbarNav">
           <ul className="navbar-nav" style={{ fontSize: '20px', fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif' }}>
             <li className="nav-item active">
-              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Home</a>
+              <Link to="/home" className="nav-link hvr-underline-from-left" style={{ color: 'white' }}>Home</Link>
             </li>
             <li className="nav-item active">
-              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>News</a>
+              <Link to="/news" className="nav-link hvr-underline-from-left"  style={{ color: 'white' }}>News</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Prices</a>
+              <Link to="/prices" className="nav-link hvr-underline-from-left"  style={{ color: 'white' }}>Prices</Link>
             </li>
             <li className="nav-item dropdown" onMouseEnter={toggleDropdown} onMouseLeave={toggleDropdown}>
               <a className="nav-link dropdown-toggle hvr-underline-from-left" href="/" id="navbarDropdown" role="button" data-toggle="dropdown" aria-haspopup="true" aria-expanded={isDropdownOpen} style={{ color: 'white' }}>
                 Transactions
               </a>
               <div className={`dropdown-menu ${isDropdownOpen ? 'show' : ''}`} aria-labelledby="navbarDropdown" style={{backgroundColor: "blue"}}>
-                <a className="dropdown-item" href="/" style={{ color: 'black' }}>Current Transactions</a>
-                <a className="dropdown-item" href="/" style={{ color: 'black' }}>Transaction Records</a>
+                <Link to="/ct" className="dropdown-item" href="/" style={{ color: 'black' }}>Current Transactions</Link>
+                <Link to="/tr" className="dropdown-item"  style={{ color: 'black' }}>Transaction Records</Link>
               </div>
             </li>
             <li className="nav-item">
-              <a className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Contact</a>
+              <Link to="/contact" className="nav-link hvr-underline-from-left" href="/" style={{ color: 'white' }}>Contact</Link>
             </li>
           </ul>
         </div>
         <div className="ml-auto" style={{ fontSize: '20px', fontFamily: 'Gill Sans, Gill Sans MT, Calibri, Trebuchet MS, sans-serif' }}>
           <ul className="navbar-nav">
             <li className="nav-item">
-              <a className="nav-link hvr-underline-from-left" href="/"style={{ color: 'white' }}>Log In</a>
+              <Link to="/login-register" className="nav-link hvr-underline-from-left" href="/"style={{ color: 'white' }}>Log In</Link>
             </li>
             <li className="nav-item">
-              <a className="nav-link hvr-underline-from-left" href="/"style={{ color: 'white', borderRadius: '10px' }}>Register</a>
+              <Link to="/login-register" className="nav-link hvr-underline-from-left" href="/"style={{ color: 'white', borderRadius: '10px' }}>Register</Link>
             </li>
           </ul>
         </div>
